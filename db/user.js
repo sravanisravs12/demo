d08f1c174a84832c4b965550fbdfdd880be2b0a7
sravanisravs12 <sravani.motupalli01@gmail.com>
2024-05-06 15:15:25 +0530

    first commit

---
+++ db/user.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+require("dotenv").config()
+const todoSchema = require("./todoSchema");
+const userSchema = require("./userSchema");
+
+const DB_NAME = process.env.DB_NAME || "todoApp";
+const URI = process.env.MONGO_URI || "mongodb+srv://temp:temp@cluster0.wkea2ju.mongodb.net";
+const MONGO_URI = `${URI}/${DB_NAME}`;
+mongoose.connect(MONGO_URI).then(() => console.log("Connected")).catch((err) => console.log(err))
+
+const users = mongoose.model('user', userSchema, 'users');
+const todos = mongoose.model('todo', todoSchema, 'todos');
+
+module.exports = {users, todos};
